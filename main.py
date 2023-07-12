00b5ec9ceeec634317697c1bf0e8634b1aab6dcd
forartsake <106870402+forartsake@users.noreply.github.com>
2023-07-12 12:28:39 +0200

    updated script

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 import sys
 import pandas as pd
-from datetime import datetime
-
 
 class CSVFileCombiner:
     def __init__(self, file1, file2, output_file):
@@ -21,14 +19,29 @@ class CSVFileCombiner:
             df1 = pd.read_csv(self.file1)
             df2 = pd.read_csv(self.file2)
 
-            df_merged = pd.concat([df1, df2]) \
-                .sort_values('updated', ascending=False) \
-                .drop_duplicates(
-                'ID') \
-                .sort_values('ID')
-            df_merged['updated'] = datetime.now().strftime('%Y-%m-%d %H:%M:%S.%f')
-            df_merged.to_csv(self.output_file, index=False)
+            df3 = pd.DataFrame(columns=df1.columns)
 
+            for index, row in df1.iterrows():
+                df1_value_id = row['ID']
+                matching_row = df2[df2['ID'] == df1_value_id]
+
+                if not matching_row.empty:
+                    if not row.drop('updated').equals(matching_row.iloc[0].drop('updated')):
+                        new_row = matching_row.iloc[0].copy()
+                        df3 = pd.concat([df3, pd.DataFrame([new_row])], ignore_index=True)
+                    else:
+                        df3 = pd.concat([df3, pd.DataFrame([row])], ignore_index=True)
+                else:
+                    df3 = pd.concat([df3, pd.DataFrame([row])], ignore_index=True)
+
+            for index, row in df2.iterrows():
+                df2_value_id = row['ID']
+
+                matching_row = df3[df3['ID'] == df2_value_id]
+                if matching_row.empty:
+                    df3 = pd.concat([df3, pd.DataFrame([row])], ignore_index=True)
+
+            df3.to_csv(self.output_file, index=False)
             print("Script has been executed successfully.")
 
         except Exception as e:
@@ -36,7 +49,6 @@ class CSVFileCombiner:
 
 
 if __name__ == "__main__":
-
     if len(sys.argv) < 4:
         print("Please specify the path to the first file as the first argument, the path"
               " from to the second file as the second argument, and the path for the new"
@@ -48,3 +60,4 @@ if __name__ == "__main__":
 
         csv_combiner = CSVFileCombiner(file1, file2, output_file)
         csv_combiner.combine_csv_files()
+
